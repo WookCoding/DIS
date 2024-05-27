3de5014d63bcd904d87b2f47e8cf500b83327955
WookCoding <dnrwhddla@gmail.com>
2024-05-27 19:35:33 +0900

    generate project

---
+++ src/test/java/com/app/dis/DisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.dis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
