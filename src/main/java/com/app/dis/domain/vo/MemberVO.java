66ec24ae30a3b7825b80c6a9991f094ce4510990
WookCoding <dnrwhddla@gmail.com>
2024-05-29 01:34:13 +0900

    back normal upload

---
+++ src/main/java/com/app/dis/domain/vo/MemberVO.java
@@ -0,0 +1,22 @@
+package com.app.dis.domain.vo;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Getter
+@Builder
+@ToString
+public class MemberVO {
+
+    private int memberId;
+    private String memberIdentification;
+    private String memberPassword;
+    private String memberName;
+    private String memberNickname;
+    private String memberPhoneNumber;
+    private Date memberRegisterDate;
+
+}
