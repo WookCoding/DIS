a1643261839a22a6d0ce639432d7cb73f6c013be
WookCoding <dnrwhddla@gmail.com>
2024-05-29 23:53:36 +0900

    sms 발송 서비스

--- src/main/java/com/app/dis/service/CheckService.java
+++ src/main/java/com/app/dis/service/CheckService.java
@@ -1,5 +1,8 @@
 package com.app.dis.service;
 
+import com.app.dis.domain.dao.SmsKeyDAO;
+import com.app.dis.domain.vo.SmsKeyVO;
+import lombok.RequiredArgsConstructor;
 import net.nurigo.java_sdk.api.Message;
 import net.nurigo.java_sdk.exceptions.CoolsmsException;
 import org.json.simple.JSONObject;
@@ -9,13 +12,18 @@ import java.util.HashMap;
 import java.util.Random;
 
 @Service
+@RequiredArgsConstructor
 public class CheckService {
+
+    private final SmsKeyDAO smsKeyDAO;
 
 //    인증번호 발송
     public String sendCertificationNumber(String phoneNumber){
-        String apiKey = "";
-        String apiSecret = "";
-        String fromNumber = "";
+        SmsKeyVO smsKeyVO = smsKeyDAO.getSmsKey();
+
+        String apiKey = smsKeyVO.getSmsApiKey();
+        String apiSecret = smsKeyVO.getSmsSecretKey();
+        String fromNumber = smsKeyVO.getSmsSendPhoneNumber();
         String code = "";
 
         Random random = new Random();
@@ -23,22 +31,22 @@ public class CheckService {
         for (int i = 0; i < 4; i++) {
             code += random.nextInt(10);
         }
-        Message coolsms = new Message(apiKey, apiSecret);
-
-        HashMap<String, String> params = new HashMap<>();
-        params.put("to", phoneNumber);
-        params.put("from", fromNumber);
-        params.put("type", "sms");
-        params.put("text", "[DIS] 인증번호 "+ code +" 를 입력하세요.");
-        params.put("app_version", "test app 1.2"); // application name and version
-
-        try {
-            JSONObject obj = coolsms.send(params);
-            System.out.println(obj.toString());
-        } catch (CoolsmsException e) {
-            System.out.println(e.getMessage());
-            System.out.println(e.getCode());
-        }
+//        Message coolsms = new Message(apiKey, apiSecret);
+//
+//        HashMap<String, String> params = new HashMap<>();
+//        params.put("to", phoneNumber);
+//        params.put("from", fromNumber);
+//        params.put("type", "sms");
+//        params.put("text", "[DIS] 인증번호 "+ code +" 를 입력하세요.");
+//        params.put("app_version", "test app 1.2"); // application name and version
+//
+//        try {
+//            JSONObject obj = coolsms.send(params);
+//            System.out.println(obj.toString());
+//        } catch (CoolsmsException e) {
+//            System.out.println(e.getMessage());
+//            System.out.println(e.getCode());
+//        }
         return code;
     }
 }
